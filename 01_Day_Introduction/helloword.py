c3da4183c8aa049e0d3344f30caff2a3efd64d72
kristocode <kristocode@gmail.com>
2021-10-05 09:35:25 -0300

    Add comment a helloword.py

--- 01_Day_Introduction/helloword.py
+++ 01_Day_Introduction/helloword.py
@@ -13,7 +13,14 @@ print(type(10))          # Int
 print(type(3.14))        # Float
 print(type(1 + 3j))      # Complex number
 print(type('Asabeneh'))  # String
+# Python list is an ordered collection which allows to store different data type items. 
+# A list is similar to an array in JavaScript.
 print(type([1, 2, 3]))   # List
+# A Python dictionary object is an unordered collection of data in a key value pair format.
 print(type({'name':'Asabeneh'})) # Dictionary
+# A set is a collection of data types similar to list and tuple. Unlike list and tuple, 
+# set is not an ordered collection of items.
 print(type({9.8, 3.14, 2.7}))    # Set
+# A tuple is an ordered collection of different data types like list but tuples 
+# can not be modified once they are created. They are immutable.
 print(type((9.8, 3.14, 2.7)))    # Tuple
